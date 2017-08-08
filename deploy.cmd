9beaaad8d5680f19e1112c2c84f94acb484b015a
Néstor Acevedo <nestor.acevedo.romero@gmail.com>
2017-08-08 15:51:12 -0500

    Corriendo Yii en modo Producción. Sobrescribiendo el archivo main-local.php y ejecutando la migración.

--- deploy.cmd
+++ deploy.cmd
@@ -61,10 +61,51 @@ echo Ejecutando Composer
 php composer.phar install
 
 echo ------------------------------------
-echo Iniciando YIi2 en modo Produccion
+echo Iniciando Yii2 en modo Produccion
 echo ------------------------------------
 
 php init --env=Production
+
+echo ------------------------------------
+echo Sobrescribiendo main-local.php
+echo ------------------------------------
+
+@echo off
+@echo "<?php" > common/config/main-local.php
+@echo "return [" > common/config/main-local.php
+@echo "    'db' => [" > common/config/main-local.php
+@echo "                'class' => 'yii\db\Connection'," > common/config/main-local.php
+@echo "                'dsn' => 'mysql:host=localhost;dbname=openbiblio2'," > common/config/main-local.php
+@echo "                'username' => 'azure'," > common/config/main-local.php
+@echo "                'password' => 'password'," > common/config/main-local.php
+@echo "                'charset' => 'utf8'," > common/config/main-local.php
+@echo "            ]," > common/config/main-local.php
+@echo "            'mailer' => [" > common/config/main-local.php
+@echo "                'class' => 'yii\swiftmailer\Mailer'," > common/config/main-local.php
+@echo "                'viewPath' => '@common/mail'," > common/config/main-local.php
+@echo "                // send all mails to a file by default. You have to set" > common/config/main-local.php
+@echo "                // 'useFileTransport' to false and configure a transport" > common/config/main-local.php
+@echo "                // for the mailer to send real emails." > common/config/main-local.php
+@echo "                'useFileTransport' => false," > common/config/main-local.php
+@echo "                'transport' => [" > common/config/main-local.php
+@echo "                    'class' => 'Swift_SmtpTransport'," > common/config/main-local.php
+@echo "                    'host' => 'smtp.googlemail.com', // e.g. smtp.mandrillapp.com or smtp.gmail.com" > common/config/main-local.php
+@echo "                    'username' => 'nestor.acevedo.romero'," > common/config/main-local.php
+@echo "                    'password' => '_*Hynt1b@_*'," > common/config/main-local.php
+@echo "                    'port' => '587', // Port 25 is a very common port too" > common/config/main-local.php
+@echo "                    'encryption' => 'tls', // It is often used, check your provider or mail server specs" > common/config/main-local.php
+@echo "                ]," > common/config/main-local.php
+@echo "            ]," > common/config/main-local.php
+@echo "            'gridview' => ['class' => 'kartik\grid\Module']" > common/config/main-local.php
+@echo "        ]," > common/config/main-local.php
+@echo "    ];" > common/config/main-local.php
+@echo on
+
+echo ------------------------------------
+echo  Migrando la base de datos.
+echo ------------------------------------
+
+php yii migrate
 
 :: 1. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
