da0387b8971339e5d406b73438814cf3abe34120
Néstor Acevedo <nestor.acevedo.romero@gmail.com>
2017-08-08 19:54:46 -0500

    Se crea el archivo main-local.php con la configuración.

--- console/PostDeploymentActions/install.cmd
+++ console/PostDeploymentActions/install.cmd
@@ -1,20 +1,20 @@
 curl -O https://getcomposer.org/composer.phar
 
-@echo Ejecutando Composer
+echo Ejecutando Composer
 
 php composer.phar install
 
-@echo ------------------------------------
-@echo Iniciando Yii2 en modo Produccion
-@echo ------------------------------------
+echo ------------------------------------
+echo Iniciando Yii2 en modo Produccion
+echo ------------------------------------
 
 php init --env=Production
 
-@echo ------------------------------------
-@echo Sobrescribiendo main-local.php
-@echo ------------------------------------
+echo ------------------------------------
+echo Sobrescribiendo main-local.php
+echo ------------------------------------
 
-xcopy /f /y main-local.php common/config/main-local.php
+Move-Item -Path "main-local.php" -Destination "%ARTIFACTS%\wwwroot\common\config\main-local.php"
 
 echo ------------------------------------
 echo  Migrando la base de datos.
