7b605d70dc0c4687fa11249a817ecb40a64e64f7
Néstor Acevedo <nestor.acevedo.romero@gmail.com>
2018-01-03 12:59:00 -0500

    Se crean las configuraciones para aws elastic beanstalk.

--- common/config/database.php
+++ common/config/database.php
@@ -5,39 +5,19 @@ $connectstr_dbname = "";
 $connectstr_dbusername = "";
 $connectstr_dbpassword = "";
 
-// Azure MySQL in-app 
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-        // exclusivo AWS EB de prueba.
-        if (strpos($key, "RDS_HOSTNAME") !== 0) {
-            continue;
-        } else {
-            $connectstr_dbhost = filter_input(INPUT_SERVER, "RDS_HOSTNAME");
-            $connectstr_dbname = filter_input(INPUT_SERVER, "RDS_DB_NAME");
-            $connectstr_dbusername = filter_input(INPUT_SERVER, "RDS_USERNAME");
-            $connectstr_dbpassword = filter_input(INPUT_SERVER, "RDS_PASSWORD");
-        }
-    } else {
-        $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-        $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-        $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-        $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-    }
-}
-
-if (null !== $connectstr_dbhost) {
+if (!$connectstr_dbhost = filter_input(INPUT_SERVER, "RDS_HOSTNAME")) {
     $connectstr_dbhost = "localhost";
 }
 
-if (null !== $connectstr_dbname) {
+if (!$connectstr_dbname = filter_input(INPUT_SERVER, "RDS_DB_NAME")) {
     $connectstr_dbname = "openbiblio2";
 }
 
-if (null !== $connectstr_dbusername) {
+if (!$connectstr_dbusername = filter_input(INPUT_SERVER, "RDS_USERNAME")) {
     $connectstr_dbusername = "root";
 }
 
-if (null !== $connectstr_dbpassword) {
+if (!$connectstr_dbpassword = filter_input(INPUT_SERVER, "RDS_PASSWORD")) {
     $connectstr_dbpassword = "";
 }
 
--- console/config/database.php
+++ console/config/database.php
@@ -1,10 +1,34 @@
 <?php
 
+/**
+ * En aplicación de consola, no se hará uso de filter_input, sino que directamente se tomarán los valores.
+ */
+$connectstr_dbhost = "";
+$connectstr_dbname = "";
+$connectstr_dbusername = "";
+$connectstr_dbpassword = "";
+
+if (!$connectstr_dbhost = getenv("RDS_HOST_NAME")) {
+    $connectstr_dbhost = "localhost";
+}
+
+if (!$connectstr_dbname = getenv("RDS_DB_NAME")) {
+    $connectstr_dbname = "openbiblio2";
+}
+
+if (!$connectstr_dbname = getenv('RDS_USERNAME')) {
+    $connectstr_dbusername = "root";
+}
+
+if (!$connectstr_dbpassword = getenv("RDS_PASSWORD")) {
+    $connectstr_dbpassword = "";
+}
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => "mysql:host=localhost;dbname=openbiblio2",
-    'username' => 'root',
-    'password' => '',
+    'dsn' => "mysql:host=$connectstr_dbhost;dbname=$connectstr_dbname",
+    'username' => $connectstr_dbusername,
+    'password' => $connectstr_dbpassword,
     'charset' => 'utf8',
     'enableQueryCache' => false
 ];
