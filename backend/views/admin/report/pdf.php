30961927c661498e0d79d5534a24c16779b6bd13
Néstor Acevedo <nestor.acevedo.romero@gmail.com>
2018-01-03 22:30:41 -0500

    Se crean las configuraciones para aws elastic beanstalk.

--- backend/views/admin/report/pdf.php
+++ backend/views/admin/report/pdf.php
@@ -6,10 +6,6 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = Yii::t('app/reports', 'Report');
-$this->params['breadcrumbs'][] = ['label' => Yii::t("app/reports", "Reports"), 'url' => ["admin/report/index"]];
-$this->params['breadcrumbs'][] = $this->title;
-
 // take just first model in the list
 $model = $dataProvider->models[0];
 if (count($model) > 0) {
@@ -26,6 +22,10 @@ if (count($model) > 0) {
 $route = Yii::$app->request->queryParams;
 array_shift($route);
 $pdfRoute = array_merge(["admin/report/pdf"], $route);
+
+$this->title = Yii::t('app/reports', $model->name);
+$this->params['breadcrumbs'][] = ['label' => Yii::t("app/reports", "Reports"), 'url' => ["admin/report/index"]];
+$this->params['breadcrumbs'][] = $this->title;
 
 ?>
 <div class="collection-index">
