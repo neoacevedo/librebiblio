eddb94ba7c29876df4d697b558d5574d075774a8
Néstor Acevedo <nestor.acevedo.romero@gmail.com>
2018-01-09 12:54:11 -0500

    Configuración mail.

--- common/config/mail.php
+++ common/config/mail.php
@@ -6,9 +6,9 @@ return [
     'useFileTransport' => false, //for the testing purpose, you need to enable this
     'transport' => [
         'class' => 'Swift_SmtpTransport',
-        'host' => 'smtp.googlemail.com', // e.g. smtp.mandrillapp.com or smtp.gmail.com
-        'username' => 'nestor.acevedo.romero@gmail.com',
-        'password' => "Hynt1b@2017",
+        'host' => getenv('SMTP_HOST') ?: 'smtp.googlemail.com', // e.g. smtp.mandrillapp.com or smtp.gmail.com
+        'username' => getenv('USERNAME') ?: 'nestor.acevedo.romero@gmail.com',
+        'password' => getenv('PASSWORD') ?: "Hynt1b@2017",
         'port' => '587', // Port 25 is a very common port too
         'encryption' => 'tls', // It is often used, check your provider or mail server specs
     ],
--- console/config/main.php
+++ console/config/main.php
@@ -24,9 +24,9 @@ return [
             'useFileTransport' => false, //for the testing purpose, you need to enable this
             'transport' => [
                 'class' => 'Swift_SmtpTransport',
-                'host' => 'smtp.googlemail.com', // e.g. smtp.mandrillapp.com or smtp.gmail.com
-                'username' => 'nestor.acevedo.romero@gmail.com',
-                'password' => "Hynt1b@2017",
+                'host' => getenv('SMTP_HOST') ?: 'smtp.googlemail.com', // e.g. smtp.mandrillapp.com or smtp.gmail.com
+                'username' => getenv('USERNAME') ?: 'nestor.acevedo.romero@gmail.com',
+                'password' => getenv('PASSWORD') ?: "Hynt1b@2017",
                 'port' => '587', // Port 25 is a very common port too
                 'encryption' => 'tls', // It is often used, check your provider or mail server specs
             ],
@@ -55,8 +55,8 @@ return [
         ],
         'urlManager' => [
             'class' => 'yii\web\UrlManager',
-            'baseUrl' => (filter_input(INPUT_SERVER, 'SERVER_NAME') === 'localhost') ? 'http://localhost/openbiblio2/backend/web': filter_input(INPUT_SERVER, 'SERVER_NAME'), // reemplazar de manera manual por el dominio para el backend
-            'scriptUrl' => (filter_input(INPUT_SERVER, 'SERVER_NAME') === 'localhost') ? 'http://localhost/openbiblio2/backend/web': filter_input(INPUT_SERVER, 'SERVER_NAME'), // reemplazar de manera manual por el dominio para el backend
+            'baseUrl' => (filter_input(INPUT_SERVER, 'SERVER_NAME') === 'localhost') ? 'http://localhost/openbiblio2/backend/web' : filter_input(INPUT_SERVER, 'SERVER_NAME'), // reemplazar de manera manual por el dominio para el backend
+            'scriptUrl' => (filter_input(INPUT_SERVER, 'SERVER_NAME') === 'localhost') ? 'http://localhost/openbiblio2/backend/web' : filter_input(INPUT_SERVER, 'SERVER_NAME'), // reemplazar de manera manual por el dominio para el backend
             'enablePrettyUrl' => false,
             'showScriptName' => false,
             'rules' => [
