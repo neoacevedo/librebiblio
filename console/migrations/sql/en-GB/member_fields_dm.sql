d9b1ed64be47841c36b0ea8963c2956d0097e3e6
Néstor Acevedo <nestor.acevedo.romero@gmail.com>
2018-01-09 22:37:33 -0500

    Modificadas migraciones.

--- console/migrations/sql/en-GB/member_fields_dm.sql
+++ console/migrations/sql/en-GB/member_fields_dm.sql
@@ -1,2 +1,2 @@
-insert into "{{%member_fields_dm}}" values ('schoolGrade','School Grade','N');
-insert into "{{%member_fields_dm}}" values ('schoolTeacher','School Teacher','N');
+insert into {{%member_fields_dm}} values ('schoolGrade','School Grade','N');
+insert into {{%member_fields_dm}} values ('schoolTeacher','School Teacher','N');
