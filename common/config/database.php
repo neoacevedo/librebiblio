e440e82a00d7be8f0c4f432a1f2307f3457a31b6
Néstor Acevedo <nestor.acevedo.romero@gmail.com>
2018-01-13 14:46:41 -0500

    modificado para ambiente de desarrollo o producción.

--- common/config/database.php
+++ common/config/database.php
@@ -1,9 +1,9 @@
 <?php
 
-$connectstr_dbhost = filter_input(INPUT_SERVER, "RDS_HOSTNAME") ?: "localhost";
-$connectstr_dbname = filter_input(INPUT_SERVER, "RDS_DB_NAME") ?: "openbiblio2";
-$connectstr_dbusername = filter_input(INPUT_SERVER, "RDS_USERNAME") ?: "root";
-$connectstr_dbpassword = filter_input(INPUT_SERVER, "RDS_PASSWORD") ?: "";
+$connectstr_dbhost = filter_input(INPUT_SERVER, "RDS_HOSTNAME");
+$connectstr_dbname = filter_input(INPUT_SERVER, "RDS_DB_NAME");
+$connectstr_dbusername = filter_input(INPUT_SERVER, "RDS_USERNAME");
+$connectstr_dbpassword = filter_input(INPUT_SERVER, "RDS_PASSWORD");
 
 return [
     'class' => 'yii\db\Connection',
