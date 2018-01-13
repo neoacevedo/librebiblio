21781cc99ec41d807ecb8dbeb1c49068b5d87289
Néstor Acevedo <nestor.acevedo.romero@gmail.com>
2018-01-13 15:20:05 -0500

    modificado para ambiente de desarrollo o producción.

--- console/config/database.php
+++ console/config/database.php
@@ -1,9 +1,9 @@
 <?php
 
-$connectstr_dbhost = getenv("RDS_HOSTNAME");
-$connectstr_dbname = getenv("RDS_DB_NAME");
-$connectstr_dbusername = getenv("RDS_USERNAME");
-$connectstr_dbpassword = getenv("RDS_PASSWORD");
+$connectstr_dbhost = get_cfg_var("RDS_HOSTNAME");
+$connectstr_dbname = get_cfg_var("RDS_DB_NAME");
+$connectstr_dbusername = get_cfg_var("RDS_USERNAME");
+$connectstr_dbpassword = get_cfg_var("RDS_PASSWORD");
 
 return [
     'class' => 'yii\db\Connection',
