f007716b4bbeca605ebb88fadbb24e5145e142ee
Néstor Acevedo <nestor.acevedo.romero@gmail.com>
2018-01-13 15:40:33 -0500

    modificado para ambiente de desarrollo o producción.

--- console/config/database.php
+++ console/config/database.php
@@ -1,9 +1,10 @@
 <?php
-
-$connectstr_dbhost = get_cfg_var("RDS_HOSTNAME");
-$connectstr_dbname = get_cfg_var("RDS_DB_NAME");
-$connectstr_dbusername = get_cfg_var("RDS_USERNAME");
-$connectstr_dbpassword = get_cfg_var("RDS_PASSWORD");
+$eb_config = sprintf("%s", `/opt/elasticbeanstalk/bin/get-config environment`);
+$config = json_decode($eb_config);
+$connectstr_dbhost = $config->RDS_HOSTNAME;
+$connectstr_dbname = $config->RDS_DB_NAME;
+$connectstr_dbusername = $config->RDS_USERNAME;
+$connectstr_dbpassword = $config->RDS_PASSWORD;
 
 return [
     'class' => 'yii\db\Connection',
