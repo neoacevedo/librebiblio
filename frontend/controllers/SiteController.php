21c93512c14853adacff0b9eec16a130823c6c21
Néstor Acevedo <nestor.acevedo.romero@gmail.com>
2018-03-11 12:25:06 -0500

    Corrección en el RBAC para contact

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -29,7 +29,7 @@ class SiteController extends Controller {
                 'class' => AccessControl::className(),
                 'rules' => [
                     [
-                        'actions' => ['login', 'error', 'index'],
+                        'actions' => ['login', 'error', 'index', 'contact'],
                         'allow' => true,
                     ],
                     [
