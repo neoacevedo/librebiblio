aba4f2d02991059c648a3f905a2dcc96f109c187
Néstor Acevedo <nestor.acevedo.romero@gmail.com>
2018-04-22 13:46:21 -0500

    Actualizaciones - Generado carro de compra para los préstamos. - Modificación de los reportes: se separa la vista para cada uno. - Corrección en la vista del material bibliográfico.

--- backend/views/cataloging/biblio/view.php
+++ backend/views/cataloging/biblio/view.php
@@ -15,27 +15,27 @@ $usmarc = [
     [
         'attribute' => 'topic1',
         'value' => $model->topic1,
-        'label' => \Yii::t('app', 'Topic1')
+        'label' => \Yii::t('biblio', 'Topic1')
     ],
     [
         'attribute' => 'topic2',
         'value' => $model->topic2,
-        'label' => \Yii::t('app', 'Topic2')
+        'label' => \Yii::t('biblio', 'Topic2')
     ],
     [
         'attribute' => 'topic3',
         'value' => $model->topic3,
-        'label' => \Yii::t('app', 'Topic3')
+        'label' => \Yii::t('biblio', 'Topic3')
     ],
     [
         'attribute' => 'topic4',
         'value' => $model->topic4,
-        'label' => \Yii::t('app', 'Topic4')
+        'label' => \Yii::t('biblio', 'Topic4')
     ],
     [
         'attribute' => 'topic5',
         'value' => $model->topic5,
-        'label' => \Yii::t('app', 'Topic5')
+        'label' => \Yii::t('biblio', 'Topic5')
     ]
 ];
 foreach ($model->biblioFields as $biblioField) {
@@ -64,95 +64,129 @@ foreach ($model->biblioFields as $biblioField) {
         ])
         ?>
     </p>
-
-    <?=
-    DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'created_at',
-            'updated_at',
-            [
-                'attribute' => 'user',
-                'value' => $model->user->username,
-                'label' => \Yii::t('app', 'Updated by')
-            ],
-            [
-                'attribute' => 'materialType',
-                'value' => $model->materialType->description,
-                'label' => 'Material'
-            ],
-            [
-                'attribute' => 'collection',
-                'value' => $model->collection->description,
-                'label' => Yii::t('app', 'Collection')
-            ],
-            [
-                'attribute' => 'call_nmbr1',
-                'value' => "$model->call_nmbr1 $model->call_nmbr2 $model->call_nmbr3",
-                'label' => Yii::t('app', 'Call Nmbr1')
-            ],
-            'title:ntext',
-            'title_remainder:ntext',
-            [
-                'attribute' => 'image_file',
-                'value' => function($model) {
-                    return Html::img(Yii::$app->storage->getUrl("images/covers/{$model->image_file}"), ['alt' => $model->title,
+    <div class="box">
+        <div class="box-body">
+            <?=
+            DetailView::widget([
+                'model' => $model,
+                'attributes' => [
+                    'id',
+                    'created_at',
+                    'updated_at',
+                    [
+                        'attribute' => 'user',
+                        'value' => $model->user->username,
+                        'label' => \Yii::t('app', 'Updated by')
+                    ],
+                    [
+                        'attribute' => 'materialType',
+                        'value' => $model->materialType->description,
+                        'label' => 'Material'
+                    ],
+                    [
+                        'attribute' => 'collection',
+                        'value' => $model->collection->description,
+                        'label' => Yii::t('app', 'Collection')
+                    ],
+                    [
+                        'attribute' => 'call_nmbr1',
+                        'value' => "$model->call_nmbr1 $model->call_nmbr2 $model->call_nmbr3",
+                        'label' => Yii::t('biblio', 'Call Nmbr1')
+                    ],
+                    'title:ntext',
+                    'title_remainder:ntext',
+                    [
+                        'attribute' => 'image_file',
+                        'value' => function($model) {
+                            return Html::img(Yii::$app->storage->getUrl("images/covers/{$model->image_file}"), ['alt' => $model->title,
                                         'title' => $model->title,
                                         'class' => 'image-thumbnail center-block',
                                         'style' => 'width: 140px']);
-                },
-                'format' => 'raw'
-            ],
-            'responsibility_stmt:ntext',
-            'author:ntext',
-            [
-                'attribute' => 'opac_flg',
-                'value' => function($model) {
-                    return ($model->opac_flg == 1) ? Yii::t('app', 'Yes') : Yii::t('app', 'No');
-                },
-            ]
-        ],
-    ])
-    ?>
-    <div class="row">
-        <div class="col-md-12 col-sm-12">
-            <h4><?= Yii::t('app', 'Bibliography Copy Information') ?></h4>
+                        },
+                        'format' => 'raw'
+                    ],
+                    'responsibility_stmt:ntext',
+                    'author:ntext',
+                    [
+                        'attribute' => 'opac_flg',
+                        'value' => function($model) {
+                            return ($model->opac_flg == 1) ? Yii::t('app', 'Yes') : Yii::t('app', 'No');
+                        },
+                    ]
+                ],
+                'options' => ['class' => 'table table-striped table-bordered table-responsive']
+            ])
+            ?>
         </div>
     </div>
-    <?php
-    $biblioCopySearch = new \common\models\BiblioCopySearch();
-    $biblioCopy = $biblioCopySearch->search(['BiblioCopySearch' => ['bibid' => $model->id]]);
+    <div class="box">
+        <div class="box-header">
+            <h3><?= Yii::t('app', 'Bibliography Copy Information') ?></h3>
+        </div>
+        <div class="box-body">
+            <?php
+            $biblioCopySearch = new \common\models\BiblioCopySearch();
+            $biblioCopy = $biblioCopySearch->search(['BiblioCopySearch' => ['bibid' => $model->id]]);
 
-    echo GridView::widget([
-        "dataProvider" => $biblioCopy,
-        'summary' => '',
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            'barcode_nmbr',
-            'copy_desc',
-            [
-                'attribute' => 'status_cd',
-                'value' => function($model) {
-                    return common\models\BiblioStatusDm::findOne(['code' => $model->status_cd])->description;
-                },
-                'label' => Yii::t('app', 'Status')
-            ],
-            'status_begin_dt',
-            'due_back_dt',
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]);
-    ?>
-    <div class="row">
-        <div class="col-md-12 col-sm-12">
-            <h4><?= Yii::t('app', 'Additional Bibliographic Information') ?></h4>
+            echo GridView::widget([
+                "dataProvider" => $biblioCopy,
+                'summary' => '',
+                'columns' => [
+                    ['class' => 'yii\grid\SerialColumn'],
+                    'barcode_nmbr',
+                    'copy_desc',
+                    [
+                        'attribute' => 'status_cd',
+                        'value' => function($model) {
+                            return common\models\BiblioStatusDm::findOne(['code' => $model->status_cd])->description;
+                        },
+                        'label' => Yii::t('app', 'Status')
+                    ],
+                    'status_begin_dt',
+                    'due_back_dt',
+                    [
+                        'class' => 'yii\grid\ActionColumn',
+                        'template' => '{view}&nbsp;&nbsp;{update}&nbsp;&nbsp;{delete}',
+                        'buttons' => [
+                            'view' => function ($url, $model) {
+                                return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', ['biblio-copy/view', 'id' => $model->id, 'bibid' => $model->bibid], [
+                                            'title' => Yii::t('app', 'View'),
+                                ]);
+                            },
+                            'update' => function ($url, $model) {
+                                return Html::a('<span class="glyphicon glyphicon-pencil"></span>', ['biblio-copy/update', 'id' => $model->id, 'bibid' => $model->bibid], [
+                                            'title' => Yii::t('app', 'Update'),
+                                ]);
+                            },
+                            'delete' => function ($url, $model) {
+                                return Html::a('<span class="glyphicon glyphicon-trash"></span>', ['biblio-copy/delete', 'id' => $model->id, 'bibid' => $model->bibid], [
+                                            'title' => Yii::t('app', 'Delete'),
+                                            'data' => [
+                                                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                                                'pjax' => 0,
+                                            ],
+                                ]);
+                            }
+                        ],
+                    ],
+                ],
+                'options' => ['class' => 'table table-striped table-bordered table-responsive']
+            ]);
+            ?>
         </div>
     </div>
-    <?=
-    DetailView::widget([
-        "model" => $model,
-        "attributes" => $usmarc
-    ]);
-    ?>
+    <div class="box">
+        <div class="box-header">
+            <h3><?= Yii::t('app', 'Additional Bibliographic Information') ?></h3>
+        </div>
+        <div class="box-body">
+            <?=
+            DetailView::widget([
+                "model" => $model,
+                "attributes" => $usmarc,
+                'options' => ['class' => 'table table-striped table-bordered table-responsive']
+            ]);
+            ?>
+        </div>
+    </div>
 </div>
