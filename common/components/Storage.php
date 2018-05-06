ec76f4d7820c515593570e1d49d1a2331ae0d40a
Néstor Acevedo <nestor.acevedo.romero@gmail.com>
2018-05-05 19:16:59 -0500

    Corrección en Storage.

--- common/components/Storage.php
+++ common/components/Storage.php
@@ -135,7 +135,7 @@ class Storage extends \yii\base\BaseObject {
             case self::LOCAL:
                 // Predefinido.
             default:
-                return \Yii::$app->urlManagerFrontend->baseUrl.$file;
+                return \Yii::$app->urlManagerFrontend->baseUrl."/".$file;
         }
     }
 
