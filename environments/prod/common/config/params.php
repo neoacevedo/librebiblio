85927257c132b552b4d6194cb2b93e359dbb1c44
Néstor Acevedo <nestor.acevedo.romero@gmail.com>
2018-05-25 16:20:20 -0500

    Actualizaciones - Modificando algoritmo de actualización.

--- environments/prod/common/config/params.php
+++ environments/prod/common/config/params.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @link https://www.neoacevedo.co
+ * @copyright Copyright (c) 2018 Néstor Acevedo
+ * @license https://www.neoacevedo.co/license
+ */
 return [
     'adminEmail' => getenv('adminEmail'),
     'supportEmail' => getenv('supportEmail'),
