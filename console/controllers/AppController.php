61c1ddc8a2957a76fc6beca4395a94d9d561df26
Néstor Acevedo <nestor.acevedo.romero@gmail.com>
2018-05-26 12:37:16 -0500

    Actualizaciones - Modificando algoritmo de actualización. Las actualizaciones ahora se hacen con el refresh_token

--- console/controllers/AppController.php
+++ console/controllers/AppController.php
@@ -34,55 +34,58 @@ class AppController extends Controller
      * (Repositorios privados, archivos comprimidos en almacenamiento privado, etc).
      * @param string $token Token de autorización.
      */
-    public function actionUpdate(string $token) {
-        $loginPassw = \Yii::$app->params['updateKey'] . ":" . \Yii::$app->params['updateSecret'];
-        $ch = curl_init("https://bitbucket.org/site/oauth2/access_token");
+    public function actionUpdate(string $token) 
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://bitbucket.org/site/oauth2/access_token");
         curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_PROXYTYPE, 'HTTPS');
-        curl_setopt($ch, CURLOPT_PROXYUSERPWD, $loginPassw);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, [
+            "client_id" => \Yii::$app->params['updateKey'],
+            "client_secret" => \Yii::$app->params['updateSecret'],
             "grant_type" => "refresh_token",
             "refresh_token" => $token]);
 
         $response = curl_exec($ch);
         curl_close($ch);
         
-        $auth = \GuzzleHttp\json_decode($response);
+        $auth = \json_decode($response);
         
-        $cmd = "git init && git remote add origin https://x-token-auth:{$auth->access_token}@bitbucket.org/nacevedo/openbiblio2.git && "
-                . "git fetch --all && git reset --hard origin/master";
+        $cmd = "git init && "
+                . "git remote add origin https://x-token-auth:{$auth->access_token}@bitbucket.org/nacevedo/openbiblio2.git "
+                . "&& git fetch --all && git reset --hard origin/master";
 
-        echo $this->ansiFormat("Antes de continuar, verifique que haya hecho un respaldo del sitio web (archivos y base de datos).\n"
+        echo $this->ansiFormat("Antes de continuar, verifique que haya hecho un respaldo del sitio web "
+                . "(archivos y base de datos).\n"
                 . "Este procedimiento reemplazará cualquier cambio hecho en el código fuente.\n"
                 . "Proceda luego con la actualización y ejecute una nueva migración de BD.\n\n", Console::BG_GREEN, Console::BOLD);
         
         if ($this->confirm("¿Desea continuar / Do you want to proceed?")) {
             if ($this->isEnabled('shell_exec')) {
-                $shell_exec = shell_exec($cmd);
+                $shell_exec = \shell_exec($cmd);
                 echo $this->ansiFormat($shell_exec . "\n", Console::BG_GREEN, \yii\helpers\Console::NORMAL);
                 echo $this->ansiFormat("copiando params.php...\n");
                 if (YII_ENV_PROD) {
-                    if (!copy(__DIR__ . '/../../environments/prod/common/config/params.php', __DIR__ . '/../../common/config/params.php')) {
+                    if (!\copy(__DIR__ . '/../../environments/prod/common/config/params.php', __DIR__ . '/../../common/config/params.php')) {
                         echo $this->ansiFormat("No se puede copiar common/config/params.php. Esto no afecta su entorno actual.\n");
                     }
 
-                    if (!copy(__DIR__ . '/../../environments/prod/console/config/params.php', __DIR__ . '/../../console/config/params.php')) {
+                    if (!\copy(__DIR__ . '/../../environments/prod/console/config/params.php', __DIR__ . '/../../console/config/params.php')) {
                         echo $this->ansiFormat("No se puede copiar console/config/params.php. Esto no afecta su entorno actual.\n");
                     }
                 } elseif (YII_ENV_DEV) {
-                    if (!copy(__DIR__ . '/../../environments/dev/common/config/params-local.php', __DIR__ . '/../../common/config/params-local.php')) {
+                    if (!\copy(__DIR__ . '/../../environments/dev/common/config/params-local.php', __DIR__ . '/../../common/config/params-local.php')) {
                         echo $this->ansiFormat("No se puede copiar common/config/params-local.php. Esto no afecta su entorno actual.\n");
                     }
 
-                    if (!copy(__DIR__ . '/../../environments/dev/console/config/params-local.php', __DIR__ . '/../../console/config/params-local.php')) {
+                    if (!\copy(__DIR__ . '/../../environments/dev/console/config/params-local.php', __DIR__ . '/../../console/config/params-local.php')) {
                         echo $this->ansiFormat("No se puede copiar console/config/params-local.php. Esto no afecta su entorno actual.\n");
                     }
                 }
-                shell_exec("rm -rf " . __DIR__ . '/../../.git');
+                \shell_exec("rm -rf " . __DIR__ . '/../../.git');
             } else {
-                echo $this->ansiFormat("shell_exec no está habilitado. Por favor solicite una actualización por medio de un correo electrónico.\n", Console::BG_RED, Console::BOLD);
+                echo $this->ansiFormat("shell_exec no está habilitado. "
+                        . "Por favor solicite una actualización por medio de un correo electrónico.\n", Console::BG_RED, Console::BOLD);
             }
         }
     }
@@ -94,7 +97,8 @@ class AppController extends Controller
      * @param string $func
      * @return bool
      */
-    private function isEnabled(string $func) {
+    private function isEnabled(string $func) 
+    {
         return is_callable($func) && false === stripos(ini_get('disable_functions'), $func);
     }
 
--- environments/dev/common/config/params-local.php
+++ environments/dev/common/config/params-local.php
@@ -11,7 +11,7 @@ return [
     // caducidad del token de renovación de la contraseña.
     'user.passwordResetTokenExpire' => 3600,
     'preferredLanguages' => ['es-CO'],
-    'updateKey' => 'zxLxXJvZrUkKz8LuSJ', // key del OAuth
-    'updateSecret' => 'nWF3gz2hjxXeETKkSLYtGbSRTQD7Qf22', // secret key del OAuth
-    'version' => '2.18.5.25',
+    'updateKey' => 'AFAF2ZeJ7QYSNfeB2h', // key del OAuth
+    'updateSecret' => 'X64BEuBF3KXGfwbdLzTyWefz8gbPPRFZ', // secret key del OAuth
+    'version' => '2.18.5.26',
 ];
