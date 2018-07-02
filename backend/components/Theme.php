176e3e3c5d9d9035f098e809dba621bc069b380c
Néstor Acevedo <nestor.acevedo.romero@gmail.com>
2018-07-02 15:19:07 -0500

    2018-07-02 - Se eliminan las vistas del directorio `views` en el backend - Se crea el compoente `Theme` en el backend - Se crea el archivo web.config para IIS

---
+++ backend/components/Theme.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace backend\components;
+
+/**
+ * @inheritdoc
+ */
+class Theme extends \yii\base\Theme
+{
+
+    /**
+     * Theme folder name
+     *
+     * @var string
+     */
+    public $theme;
+
+    public function init()
+    {
+        parent::init();
+
+        $theme = \common\models\Theme::find()->where(['active' => 1, 'frontend' => 0])->one();
+        if (!$theme) {
+            $this->theme = 'AdminLTE';
+        } else {
+            $this->theme = $theme->name;
+        }
+
+        $this->basePath = '@app/themes/' . $this->theme;
+        $this->baseUrl = '@web/themes/' . $this->theme;
+        $this->pathMap = [
+            '@app/views' => '@app/themes/' . $this->theme,
+        ];
+
+        // configurar el tema en la sesión        
+        \Yii::$app->session->set('backend-skin', $theme->skin);
+    }
+
+}
